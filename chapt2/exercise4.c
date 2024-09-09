232bf8797e1149eb7dff7e18ef6e38e7a003bdcf
Kelsey A <d.chan42@gmail.com>
2024-09-09 15:55:01 -0400

    adding chapter 2 examples and exercises

---
+++ chapt2/exercise4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// Exercise 4 from the book
+
+int main(void)
+{
+    int value1, value2, diff;
+
+    value1 = 87;
+    value2 = 15;
+    diff = value1 - value2;
+
+
+    printf("The difference of %i and %i is %i.\n", value1, value2, diff);
+    return 0;
+}
---
+++ chapt2/prog1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* tells the program about printf */
+
+int main (void)
+{
+    printf("Programming is fun.\n");
+    printf("And programming in C is even more fun.\n");
+
+    return 0;
+}
